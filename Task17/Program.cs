20712ebf1922230e4f657215f410233ac9d1508e
Nikita <gorbatenko.nik@bk.ru>
2023-03-12 23:15:21 +0700

    нахождение четверти в которой лежит координата

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 17. Напишите программу, которая 
+// 1 принимает на вход координаты точки (X и Y),
+// 2 причём X ≠ 0 и Y ≠ 0
+// 3 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите кординаты точки");
+Console.WriteLine("X: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+string result = quarter > 0
+                ? $"Указанные координаты соответствуют четверти -> {quarter}"
+                : "Введены некоректные координаты";
+Console.WriteLine(result);
+
+int Quarter (int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
