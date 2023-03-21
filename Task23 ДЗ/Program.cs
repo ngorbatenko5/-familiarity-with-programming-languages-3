d516251c4610396288e5ea4247da2440cd0d5dd0
Nikita <gorbatenko.nik@bk.ru>
2023-03-21 23:10:09 +0700

    Таблица кубов от 1 до N

---
+++ Task23 ДЗ/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27. 64, 125
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+    Console.WriteLine($" {count, 3} {count * count * count, 5} ");
+    count ++;
+    }
+}
