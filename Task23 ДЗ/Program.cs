36cd19a2c85426961b0fcad0e6a5678814f21125
Nikita <gorbatenko.nik@bk.ru>
2023-03-21 23:25:02 +0700

    таблица кубов

--- Task23 ДЗ/Program.cs
+++ Task23 ДЗ/Program.cs
@@ -7,14 +7,14 @@
 Console.WriteLine("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-TableSquare(number);
+CubeSquare(number);
 
-void TableSquare(int num)
+void CubeSquare(int num)
 {
     int count = 1;
     while (count <= num)
     {
-    Console.WriteLine($" {count, 3} {count * count * count, 5} ");
+    Console.WriteLine($" {count, 3} {Math.Pow(count, 3), 5} ");
     count ++;
     }
 }
