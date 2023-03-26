a471e20c7cf85213d3552881a53bc4ac1d6e0767
Nikita <gorbatenko.nik@bk.ru>
2023-03-26 13:36:36 +0700

    возведение в степень

---
+++ Task25 ДЗ/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цикл, который принимает на 
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+
+bool ValidateExponent(int exponent) 
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Prompt("Введите основание");
+int exponent = Prompt("Введите показатель");
+if (ValidateExponent(exponent))
+{
+    Console.WriteLine($"число {powerBase} в степени  {exponent} равно {Power(powerBase, exponent)}");
+}
